c128a46caaac8db48a7aecbaa5f193fe09195fde
Gamal Hosny <gamalelctron2332@gmail.com>
2026-08-26 05:26:05 +0300

    Add admin product deletion

--- controllers/admin.js
+++ controllers/admin.js
@@ -221,8 +221,8 @@ exports.getProducts=(req, res, next) => {
 }
 
 // Delete product
-exports.postDeleteProduct=(req, res, next) => {
-    const prodId=req.body.productId;
+exports.deleteProduct=(req, res, next) => {
+    const prodId=req.params.productId;
     // req.user.destroy({where: {id: prodId}}) with MySQL
     Product.findById(prodId)
     .then(product=>{
@@ -235,12 +235,10 @@ exports.postDeleteProduct=(req, res, next) => {
     .then(()=>{
 
         console.log('Deleted Product');
-        res.redirect('/admin/products');
+        res.status(200).json({message: 'Success!'});
     })
     .catch(err=>{
-        const error = new Error(err);
-        error.httpStatusCode = 500;
-        return next(error);
+        res.status(500).json({message: 'Deleting product failed!'});
     });
     
 }
--- routes/admin.js
+++ routes/admin.js
@@ -60,7 +60,7 @@ router.post('/edit-product'
     ,isAuth
     ,adminController.postEditProduct);
 
-router.post('/delete-product',isAuth,adminController.postDeleteProduct);    
+router.delete('/product/:productId',isAuth,adminController.deleteProduct);
 
 
 
